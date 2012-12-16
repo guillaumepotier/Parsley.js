c7f36f05054247b3441f24f3e9bd4a59a8cba99b
Guillaume Potier <cobalt2760@gmail.com>
2012-12-16 23:51:48 +0100

    - second commit. added some validators, tests and a bit more implementation

--- parsley.js
+++ parsley.js
@@ -8,10 +8,86 @@
 
   "use strict";
 
+  /* VALIDATORS FUNCTIONS DEFINITION
+  * ========================= */
+  var ValidatorsFn = function ( options ) {
+  }
+
+  ValidatorsFn.prototype = {
+
+    constructor: ValidatorsFn
+
+    , notnull: function ( val ) {
+      return val.length > 0;
+    }
+
+    , notblank: function ( val ) {
+      return '' !== val.replace( /^\s+/g, '' ).replace( /\s+$/g, '' );
+    }
+
+    , required: function ( val ) {
+      return this.notnull( val ) && this.notblank( val );
+    }
+
+    , type: function ( val, type ) {
+      var regExp;
+
+      switch ( type ) {
+        case "number":
+          regExp = /^-?(?:\d+|\d{1,3}(?:,\d{3})+)?(?:\.\d+)?$/;
+          break;
+        case "digits":
+          regExp = /^\d+$/;
+          break;
+        case "alphanum":
+          regExp = /^\w+$/;
+          break;
+        case "email":
+          regExp = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i;
+          break;
+        case "url":
+          regExp = /^(https?|s?ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i;
+          break;
+        case "dateIso":
+          regExp = /^\d{4}[\/\-]\d{1,2}[\/\-]\d{1,2}$/;
+          break;
+        default:
+          return false;
+          break;
+      }
+
+      return regExp.test( val );
+    }
+
+    , minlength: function ( val, min ) {
+      return val.length >= min;
+    }
+
+    , maxlength: function ( val, max ) {
+      return val.length <= max;
+    }
+
+    , rangelength: function ( val, arrayRange ) {
+      return this.minlength( val, arrayRange[0] ) && this.maxlength( val, arrayRange[1] );
+    }
+
+    , min: function ( val, min ) {
+      return val >= min;
+    }
+
+    , max: function ( val, max ) {
+      return val <= max;
+    }
+
+    , range: function ( val, arrayRange ) {
+      return val >= arrayRange[0] && val <= arrayRange[1];
+    }
+  }
+
  /* PARSLEY PUBLIC CLASS DEFINITION
   * =============================== */
-  var Parsley = function ( element, options ) {
-    this.init( 'parsley', element, options );
+  var Parsley = function ( element, validatorsFn, options ) {
+    this.init( 'parsley', element, validatorsFn, options );
   }
 
   Parsley.prototype = {
@@ -19,29 +95,84 @@
     constructor: Parsley
 
     /* init data, bind jQuery on() actions */
-    , init: function ( type, element, options ) {
+    , init: function ( type, element, validatorsFn, options ) {
       this.type = type;
+      this.validatorsFn = validatorsFn;
+      this.registeredValidators = new Array();
       this.$element = $( element );
       this.options = this.getOptions( options );
       this.parentForm = this.$element.closest( 'form' );
-      this.$element.addClass( 'parsley-validated' );
+      this.isValid = false;
 
-      // bind parsley verification events
-      // this.$element.on( this.options.events.join( '.' + this.type + ' ') , false, $.proxy( this.persist, this ) );
+      // bind parsley validators functions for item
+      for ( var method in this.options ) {
+        method = method.toLowerCase();
+
+        if ( 'function' === typeof validatorsFn[method] ) {
+          this.registeredValidators.push( {
+              "method": method
+            , "params": this.options[method]
+          } );
+        }
+      }
+
+      // if there are validators for this field, bind verification events
+      if ( this.registeredValidators.length ) {
+        this.$element.addClass( 'parsley-validated' );
+        this.$element.on( this.options.events.join( '.' + this.type + ' ') , false, $.proxy( this.validate, this ) );
+        $( this.parentForm ).on( 'submit' , false, $.proxy( this.onSubmitValidate, this ) );
+      }
     }
 
     , getOptions: function ( options ) {
-      options = $.extend( {}, $.fn[this.type].defaults, options, this.$element.data() );
+      return $.extend( {}, $.fn[this.type].defaults, options, this.$element.data() );
+    }
 
-      return options;
+    , onSubmitValidate: function ( event ) {
+      if ( false === this.validate( true ) && 'undefined' !== typeof event) {
+        event.preventDefault();
+      }
+    }
+
+    , validate: function ( onSubmit ) {
+      var val = this.$element.val();
+
+      // do validation process if field has enough chars and not previously checked
+      if ( 'undefined' === typeof onSubmit && val.length < this.options.minChars && !( this.$element.hasClass( 'parsley-error' ) || this.$element.hasClass( 'parsley-success' ) ) ) {
+        return true;
+      }
+
+      // some binded events are redundant (change & paste for example), validate only once by field change
+      if ( this.val === this.$element.val() ) {
+        return this.isValid;
+      }
+
+      this.val = this.$element.val();
+
+      // apply all field's validations rules
+      for ( var i in this.registeredValidators ) {
+        if ( !this.validatorsFn[this.registeredValidators[i].method](val, this.registeredValidators[i].params) ) {
+          this.manageErrors();
+          return this.isValid;
+        }
+      }
+
+      this.$element.removeClass( 'parsley-error' ).addClass( 'parsley-success' );
+      this.isValid = true;
+      return this.isValid;
+    }
+
+    , manageErrors: function () {
+      this.isValid = false;
+      this.$element.removeClass( 'parsley-success' ).addClass( 'parsley-error' );
     }
   }
 
   /* PARSLEY PLUGIN DEFINITION
   * ========================= */
-
   $.fn.parsley = function ( option, fn ) {
     var options = $.extend(true, {}, $.fn.parsley.defaults, option, this.data() )
+      , validatorsFn = new ValidatorsFn()
       , returnValue = false;
 
     function bind ( self ) {
@@ -51,7 +182,7 @@
 
       // if data never binded, bind it right now!
       if ( !data ) {
-        $this.data( 'parsley', ( data = new Parsley( self, fieldOptions ) ) );
+        $this.data( 'parsley', ( data = new Parsley( self, validatorsFn, fieldOptions ) ) );
       }
 
       // here is our parsley public function accessor, currently does not support args
@@ -83,7 +214,9 @@
   $.fn.parsley.Constructor = Parsley;
 
   $.fn.parsley.defaults = {
-    inputs: 'input, textarea, select'                                                           // Default supported inputs.
+    inputs: 'input, textarea, select'                                                 // Default supported inputs.
+    , events: [ 'change', 'keyup', 'paste' ]                                       // Events list that trigger a validation
+    , minChars: 4                                                                     // Trigger validators if value >= minChars
   }
 
   /* PARSLEY DATA-API
--- tests/tests.js
+++ tests/tests.js
@@ -1,16 +1,115 @@
 "use strict";
 
+var triggerValidation = function ( idOrClass, value ) {
+  $( idOrClass ).val( value );
+  $( idOrClass ).parsley( 'onSubmitValidate' );
+}
+
 var testSuite = function () {
   describe ( 'Parsley.js test suite', function () {
+
+    /***************************************
+            Fields validators binding
+    ***************************************/
     describe ( 'Test Parsley auto binding', function () {
-      it ( 'Items inside a form validated by Parsley are binded', function () {
+      it ( 'Items with validation methods inside a form validated by Parsley are binded', function () {
         expect( $( '#input1' ).hasClass( 'parsley-validated' ) ).to.be( true );
-        expect( $( '#textarea1' ).hasClass( 'parsley-validated' ) ).to.be( true );
+        expect( $( '#textarea1' ).hasClass( 'parsley-validated' ) ).to.be( false );
       } )
-      it ( 'Items can be validated as stand-alone too', function () {
+      it ( 'Items with validation methods can be validated as stand-alone too', function () {
         expect( $( '#input2' ).hasClass( 'parsley-validated' ) ).to.be( true );
         expect( $( '#textarea2' ).hasClass( 'parsley-validated' ) ).to.be( false );
       } )
+    } )
+
+    /***************************************
+          Error & success detection
+    ***************************************/
+    describe ( 'Test Parsley error & success detection', function () {
+      it ( 'If chars < minChars, on field change, do not trigger validation', function () {
+        $( '#input2' ).val( 'foo' );
+        $( '#input1' ).trigger( $.Event( 'change' ) );
+        expect( $( '#input2' ).hasClass( 'parsley-error' ) ).to.be( false );
+        expect( $( '#input2' ).hasClass( 'parsley-success' ) ).to.be( false );
+      } )
+      it ( 'If field fail one validation test, add error class', function () {
+        triggerValidation( '#input2', 'foo@bar' );
+        expect( $( '#input2' ).hasClass( 'parsley-error' ) ).to.be( true );
+        expect( $( '#input2' ).hasClass( 'parsley-success' ) ).to.be( false );
+      } )
+      it ( 'If field verify all validation tests, add success class', function () {
+        triggerValidation( '#input2', 'foo@bar.baz' );
+        expect( $( '#input2' ).hasClass( 'parsley-error' ) ).to.be( false );
+        expect( $( '#input2' ).hasClass( 'parsley-success' ) ).to.be( true );
+      } )
+    } )
+
+    /***************************************
+                trigger events
+    ***************************************/
+    describe ( 'Test inputs events', function () {
+      var events = [ 'change', 'keyup', 'paste' ];
+        it ( 'Validators are triggered on these events: ' + events.join( ', ' ) , function () {
+          for ( var event in events ) {
+            var value = ( ( event + 1 ) % 2 ) * 1000;
+            $( '#input1' ).val( value );
+            $( '#input1' ).trigger( $.Event( events[event] ) );  
+            expect( $( '#input1' ).hasClass( 'parsley-error' ) ).to.be( value === 0 );
+          }
+        } )
+    } )
+
+    /***************************************
+                test validators
+    ***************************************/
+    describe ( 'Test validators', function () {
+      it ( 'notnull', function () {
+        triggerValidation( '#notnull', '' );
+        expect( $( '#notnull' ).hasClass( 'parsley-error' ) ).to.be( true );
+        triggerValidation( '#notnull', 'foo' );
+        expect( $( '#notnull' ).hasClass( 'parsley-success' ) ).to.be( true );
+      } )
+      it ( 'notblank', function () {
+        triggerValidation( '#notblank', '   ' );
+        expect( $( '#notblank' ).hasClass( 'parsley-error' ) ).to.be( true );
+        triggerValidation( '#notnull', 'foo' );
+        expect( $( '#notnull' ).hasClass( 'parsley-success' ) ).to.be( true );
+      } )
+      it ( 'required', function () {
+        triggerValidation( '#required', '' );
+        expect( $( '#required' ).hasClass( 'parsley-error' ) ).to.be( true );
+        triggerValidation( '#required', '  ' );
+        expect( $( '#required' ).hasClass( 'parsley-error' ) ).to.be( true );
+        triggerValidation( '#required', '  foo' );
+        expect( $( '#required' ).hasClass( 'parsley-success' ) ).to.be( true );
+      } )
+      it ( 'minlength', function () {
+        triggerValidation( '#minlength', '12345' );
+        expect( $( '#minlength' ).hasClass( 'parsley-error' ) ).to.be( true );
+        triggerValidation( '#minlength', '123456' );
+        expect( $( '#minlength' ).hasClass( 'parsley-success' ) ).to.be( true );
+      } )
+      it ( 'maxlength', function () {
+        triggerValidation( '#maxlength', '12345678' );
+        expect( $( '#maxlength' ).hasClass( 'parsley-error' ) ).to.be( true );
+        triggerValidation( '#maxlength', '12345' );
+        expect( $( '#maxlength' ).hasClass( 'parsley-success' ) ).to.be( true );
+      } )
+      it ( 'rangelength', function () {
+        triggerValidation( '#rangelength', '12345678910' );
+        expect( $( '#rangelength' ).hasClass( 'parsley-error' ) ).to.be( true );
+        triggerValidation( '#rangelength', '1234567' );
+        expect( $( '#rangelength' ).hasClass( 'parsley-success' ) ).to.be( true );
+      } )
+      it ( 'min' )
+      it ( 'max' )
+      it ( 'range' )
+      it ( 'url' )
+      it ( 'email' )
+      it ( 'digits' )
+      it ( 'dateIso' )
+      it ( 'number' )
+      it ( 'alphanum' )
     } )
   } )
 }
