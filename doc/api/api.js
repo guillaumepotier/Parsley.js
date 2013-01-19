81b9af76577d9c277859a91f7ec006e1ba2303c4
Guillaume Potier <cobalt2760@gmail.com>
2013-01-19 13:40:06 +0100

    - hard reset doc branch to master. re-added CNAME

---
+++ doc/api/api.js
@@ -0,0 +1,13 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Parsley",
+        "ParsleyField",
+        "ParsleyFieldMultiple",
+        "ParsleyForm",
+        "Validator"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
