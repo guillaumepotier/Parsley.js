e9ba25c5b9397364ce438510e3821df43211cb84
Guillaume Potier <cobalt2760@gmail.com>
2013-01-21 18:28:42 +0100

    - added window.ParsleyConfig that allows to override all defaults globally. Created localization files in /i18n/ and parsley.extra.js for extra validators. Tagged 1.2.0. Refs #12 #19

--- bin/build.sh
+++ bin/build.sh
@@ -11,6 +11,10 @@ else
   echo "** building parsley-standalone.min.js version " $1
   ruby ./bin/minify tests/resources/zepto-1.0rc1[zepto.event.data.ajax].min.js parsley.js dist/parsley-standalone.min.js $1 --force
   echo "  done!"
+
+  echo "** building parsley.extend.min.js version " $1
+  ruby ./bin/minify parsley.extend.js dist/parsley.extend.min.js $1 --force
+  echo "  done!"
 fi
 
 if [[ "$2" == "doc" ]]
