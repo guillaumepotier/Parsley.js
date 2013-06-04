24ec0ef375e36c19931c92d36e615ec776ed41d6
Guillaume Potier <cobalt2760@gmail.com>
2013-06-04 10:50:12 +0200

    - test suite reporting in -R dot now

--- bin/test-suite.sh
+++ bin/test-suite.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-mocha-phantomjs tests/index.html && mocha-phantomjs tests/index.html#jquery-min && mocha-phantomjs tests/index.html#zepto && mocha-phantomjs tests/index.html#zepto-min && mocha-phantomjs
+mocha-phantomjs tests/index.html -R dot && mocha-phantomjs tests/index.html#jquery-min -R dot && mocha-phantomjs tests/index.html#zepto -R dot && mocha-phantomjs tests/index.html#zepto-min -R dot
