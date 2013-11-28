f9caaaf1862c9aeb1dd46f0d271ed10b9c46577c
jna-unikent <jna@kent.ac.uk>
2013-11-28 12:20:25 +0000

    revert unintended change

--- parsley.js
+++ parsley.js
@@ -1457,7 +1457,7 @@
 
     for ( var i in this[ 0 ].attributes ) {
       attribute = this[ 0 ].attributes[ i ];
-      if ( attribute.specified && regex.test( attribute.name ) ) {
+      if ( null !== attribute && attribute.specified && regex.test( attribute.name ) ) {
         obj[ camelize( attribute.name.replace( namespace, '' ) ) ] = deserializeValue( attribute.value );
       }
     }
