916e796f77b82da4e6b0160a93d8dd8a289d3593
Guillaume Potier <cobalt2760@gmail.com>
2014-01-05 11:01:03 +0100

    - added ParsleyOptionsFactory UT tests

--- test/tests.js
+++ test/tests.js
@@ -12,23 +12,26 @@ require(['../src/config'], function () {
     // load then Parsley modules for UT
     require([
       'src/parsley',
+      'parsley/factory/options',
       'parsley/form',
       'parsley/field',
       'parsley/ui',
       'parsley/utils'
-    ], function (Parsley, ParsleyForm, ParsleyField, ParsleyUI, ParsleyUtils) {
+    ], function (Parsley, ParsleyOptionsFactory, ParsleyForm, ParsleyField, ParsleyUI, ParsleyUtils) {
 
       // load full parsley.js + UT
       require([
         'features/parsley',
+        'features/options',
         'features/abstract',
         'features/field',
         'features/form',
         'features/ui',
         'features/utils'
-      ], function (parsleyBase, abstract, field, form, ui, utils) {
+      ], function (parsleyBase, options, abstract, field, form, ui, utils) {
         describe('Unit Tests', function () {
           parsleyBase(Parsley);
+          options(ParsleyOptionsFactory);
           abstract(Parsley);
           field(ParsleyField, Parsley);
           form(ParsleyForm, Parsley);
