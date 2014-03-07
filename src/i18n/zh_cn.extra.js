efe211f55e34216b6967df09e44301e36cec6351
fancyoung <siyang1982@msn.com>
2014-03-07 14:47:06 +0800

    Add zh_cn i18n

---
+++ src/i18n/zh_cn.extra.js
@@ -0,0 +1,6 @@
+window.ParsleyConfig = window.ParsleyConfig || {};
+window.ParsleyConfig.i18n = window.ParsleyConfig.i18n || {};
+
+window.ParsleyConfig.i18n.zh_cn = $.extend(window.ParsleyConfig.i18n.zh_cn || {}, {
+  dateiso: "请输入正确格式的日期 (YYYY-MM-DD)."
+});
