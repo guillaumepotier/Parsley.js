657b04da3878c9b2be0e38ab4ff2ff22b13715db
Michele Gerarduzzi <mgerarduzzi@zooppa.com>
2014-03-12 20:42:00 +0100

    Add missing Italian translations

---
+++ src/i18n/it.extra.js
@@ -0,0 +1,6 @@
+window.ParsleyConfig = window.ParsleyConfig || {};
+window.ParsleyConfig.i18n = window.ParsleyConfig.i18n || {};
+
+window.ParsleyConfig.i18n.it = $.extend(window.ParsleyConfig.i18n.it || {}, {
+  dateiso: "Inserire una data valida (AAAA-MM-GG)."
+});
