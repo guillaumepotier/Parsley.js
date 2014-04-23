33293a2577de1749258f96de516ca0b20a491997
Firzenizer <firzen89@hotmail.com>
2014-04-23 13:33:50 +0300

    Finnish translation
    Finnish translation added

---
+++ src/i18n/fi.extra.js
@@ -0,0 +1,6 @@
+window.ParsleyConfig = window.ParsleyConfig || {};
+window.ParsleyConfig.i18n = window.ParsleyConfig.i18n || {};
+
+window.ParsleyConfig.i18n.fi = $.extend(window.ParsleyConfig.i18n.fi || {}, {
+  dateiso: "Sy&ouml;t&auml; oikea p&auml;iv&auml;m&auml;&auml;r&auml; (YYYY-MM-DD)."
+});
