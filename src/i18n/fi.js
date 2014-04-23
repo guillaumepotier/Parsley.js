57ec2e2c1d3720fc9f54587f0de72f71b409c6e1
Firzenizer <firzen89@hotmail.com>
2014-04-23 13:33:03 +0300

    Finnish translation
    Finnish translation added.

---
+++ src/i18n/fi.js
@@ -0,0 +1,33 @@
+// ParsleyConfig definition if not already set
+window.ParsleyConfig = window.ParsleyConfig || {};
+window.ParsleyConfig.i18n = window.ParsleyConfig.i18n || {};
+
+// Define then the messages
+window.ParsleyConfig.i18n.fi = $.extend(window.ParsleyConfig.i18n.fi || {}, {
+  defaultMessage: "Sy&ouml;tetty arvo on virheellinen.",
+  type: {
+    email:        "S&auml;hk&ouml;postiosoite on virheellinen.",
+    url:          "Url-osoite on virheellinen.",
+    number:       "Sy&ouml;t&auml; numero.",
+    integer:      "Sy&ouml;t&auml; kokonaisluku.",
+    digits:       "Sy&ouml;t&auml; ainoastaan numeroita.",
+    alphanum:     "Sy&ouml;t&auml; ainoastaan kirjaimia tai numeroita."
+  },
+  notblank:       "T&auml;m&auml; kentt&auml;&auml; ei voi j&auml;tt&auml;&auml; tyhj&auml;ksi.",
+  required:       "T&auml;m&auml; kentt&auml; on pakollinen.",
+  pattern:        "Sy&ouml;tetty arvo on virheellinen.",
+  min:            "Sy&ouml;t&auml; arvo joka on yht&auml; suuri tai suurempi kuin  %s.",
+  max:            "Sy&ouml;t&auml; arvo joka on pienempi tai yht&auml; suuri kuin %s.",
+  range:          "Sy&ouml;t&auml; arvo v&auml;lilt&auml;: %s-%s.",
+  minlength:      "Sy&ouml;tetyn arvon t&auml;ytyy olla v&auml;hint&auml;&auml;n %s merkki&auml; pitk&auml;.",
+  maxlength:      "Sy&ouml;tetty arvo saa olla enint&auml;&auml;n %s merkki&auml; pitk&auml;.",
+  length:         "Sy&ouml;tetyn arvon t&auml;ytyy olla v&auml;hint&auml;&auml;n %s ja enint&auml;&auml;n %s merkki&auml; pitk&auml;.",
+  mincheck:       "Valitse v&auml;hint&auml;&auml;n %s vaihtoehtoa.",
+  maxcheck:       "Valitse enint&auml;&auml;n %s vaihtoehtoa.",
+  check:          "Valitse %s-%s vaihtoehtoa.",
+  equalto:        "Salasanat eiv&auml;t t&auml;sm&auml;&auml;."
+});
+
+// If file is loaded after Parsley main file, auto-load locale
+if ('undefined' !== typeof window.ParsleyValidator)
+  window.ParsleyValidator.addCatalog('fi', window.ParsleyConfig.i18n.fi, true);
