d6ca154151d7307c5896f3dd15170e950e501f3e
Budi Irawan <deerawan@gmail.com>
2014-05-24 22:12:05 +0700

    change it into id

--- src/i18n/id.extra.js
+++ src/i18n/id.extra.js
@@ -1,6 +1,6 @@
 window.ParsleyConfig = window.ParsleyConfig || {};
 window.ParsleyConfig.i18n = window.ParsleyConfig.i18n || {};
 
-window.ParsleyConfig.i18n.it = $.extend(window.ParsleyConfig.i18n.it || {}, {
+window.ParsleyConfig.i18n.id = $.extend(window.ParsleyConfig.i18n.id || {}, {
   dateiso: "Harus tanggal yang valid (YYYY-MM-DD)."
 });
