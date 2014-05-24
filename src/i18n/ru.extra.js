1956855968aaa7b79d6a424658763b61aadcad49
vsn4ik <vsn4ik@gmail.com>
2014-05-24 23:49:28 +0400

    Add i18n/ru.extra.js and fix files permissions.

---
+++ src/i18n/ru.extra.js
@@ -0,0 +1,6 @@
+window.ParsleyConfig = window.ParsleyConfig || {};
+window.ParsleyConfig.i18n = window.ParsleyConfig.i18n || {};
+
+window.ParsleyConfig.i18n.ru = $.extend(window.ParsleyConfig.i18n.ru || {}, {
+  dateiso: "Это значение должно быть корректной датой (ГГГГ-ММ-ДД)."
+});
