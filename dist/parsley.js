a6be86ffea65bd25d0e5938577d7303a12edbd74
c316 <josh.bechard@gmail.com>
2014-06-19 23:12:40 -0500

    Update parsley.js
    Updated spelling mistakes

--- dist/parsley.js
+++ dist/parsley.js
@@ -656,7 +656,7 @@
     Length: function ( boundaries ) {
       this.__class__ = 'Length';
       if ( !boundaries.min && !boundaries.max )
-        throw new Error( 'Lenth assert must be instanciated with a { min: x, max: y } object' );
+        throw new Error( 'Length assert must be instantiated with a { min: x, max: y } object' );
       this.min = boundaries.min;
       this.max = boundaries.max;
       this.validate = function ( value ) {
