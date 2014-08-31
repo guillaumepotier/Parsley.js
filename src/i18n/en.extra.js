fd29e72393105d6e1bd0df01d046a48c415b1786
vsn4ik <vsn4ik@gmail.com>
2014-08-31 06:09:03 +0400

    Move messages from validator/ to i18n/.

--- src/i18n/en.extra.js
+++ src/i18n/en.extra.js
@@ -5,5 +5,9 @@ window.ParsleyConfig.i18n.en = $.extend(window.ParsleyConfig.i18n.en || {}, {
   dateiso:  "This value should be a valid date (YYYY-MM-DD).",
   minwords: "This value is too short. It should have %s words or more.",
   maxwords: "This value is too long. It should have %s words or fewer.",
-  words:    "This value length is invalid. It should be between %s and %s words long."
+  words:    "This value length is invalid. It should be between %s and %s words long.",
+  gt:       "This value should be greater.",
+  gte:      "This value should be greater or equal.",
+  lt:       "This value should be less.",
+  lte:      "This value should be less or equal."
 });
