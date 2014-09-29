d5c57b982c6fcc552c7ba15e5bcea6f5ad466121
hizam mohd <zam3858@gmail.com>
2014-09-29 22:15:20 +0800

    Create ms_MY.js and ms_MY.extra.js

---
+++ src/i18n/ms_MY.extra.js
@@ -0,0 +1,13 @@
+window.ParsleyConfig = window.ParsleyConfig || {};
+window.ParsleyConfig.i18n = window.ParsleyConfig.i18n || {};
+
+window.ParsleyConfig.i18n.ms_MY = $.extend(window.ParsleyConfig.i18n.ms_MY || {}, {
+  dateiso:  "Nilai hendaklah berbentuk tarikh yang sah (YYYY-MM-DD).",
+  minwords: "Ayat terlalu pendek. Ianya perlu sekurang-kurangnya %s patah perkataan.",
+  maxwords: "Ayat terlalu panjang. Ianya tidak boleh melebihi %s patah perkataan.",
+  words:    "Panjang ayat tidak sah. Jumlah perkataan adalah diantara  %s hingga %s patah perkataan.",
+  gt:       "Nilai lebih besar diperlukan.",
+  gte:      "Nilai hendaklah lebih besar atau sama.",
+  lt:       "Nilai lebih kecil diperlukan.",
+  lte:      "Nilai hendaklah lebih kecil atau sama."
+});
