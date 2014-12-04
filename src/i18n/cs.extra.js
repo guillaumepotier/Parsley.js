df1bc3666fa9ff013c11242c6c6d0e0d727aa7f7
Jan Čermák <jan.cermak@nic.cz>
2014-12-04 16:27:19 +0100

    added Czech i18n files

---
+++ src/i18n/cs.extra.js
@@ -0,0 +1,13 @@
+window.ParsleyConfig = window.ParsleyConfig || {};
+window.ParsleyConfig.i18n = window.ParsleyConfig.i18n || {};
+
+window.ParsleyConfig.i18n.cs = $.extend(window.ParsleyConfig.i18n.cs || {}, {
+  dateiso:  "Tato položka musí být datum ve formátu RRRR-MM-DD.",
+  minwords: "Tato položka je příliš krátká. Musí mít délku nejméně %s slov.",
+  maxwords: "Tato položka je příliš dlouhá. Musí mít délku nejvíce %s slov.",
+  words:    "Tato položka má špatnou délku. Musí být od %s do %s slov dlouhá.",
+  gt:       "Tato hodnota musí být větší.",
+  gte:      "Tato hodnota musí být větší nebo rovna.",
+  lt:       "Tato hodnota musí být menší.",
+  lte:      "Tato hodnota musí být menší nebo rovna."
+});
