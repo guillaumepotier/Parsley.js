1b6cd56ae77c6c596e46199b8650f131d6b56336
Jan Čermák <jan.cermak@nic.cz>
2014-12-04 16:49:00 +0100

    some messages in cs i18n made less verbose

--- src/i18n/cs.extra.js
+++ src/i18n/cs.extra.js
@@ -3,9 +3,9 @@ window.ParsleyConfig.i18n = window.ParsleyConfig.i18n || {};
 
 window.ParsleyConfig.i18n.cs = $.extend(window.ParsleyConfig.i18n.cs || {}, {
   dateiso:  "Tato položka musí být datum ve formátu RRRR-MM-DD.",
-  minwords: "Tato položka je příliš krátká. Musí mít délku nejméně %s slov.",
-  maxwords: "Tato položka je příliš dlouhá. Musí mít délku nejvíce %s slov.",
-  words:    "Tato položka má špatnou délku. Musí být od %s do %s slov dlouhá.",
+  minwords: "Tato položka musí mít délku nejméně %s slov.",
+  maxwords: "Tato položka musí mít délku nejvíce %s slov.",
+  words:    "Tato položka musí být od %s do %s slov dlouhá.",
   gt:       "Tato hodnota musí být větší.",
   gte:      "Tato hodnota musí být větší nebo rovna.",
   lt:       "Tato hodnota musí být menší.",
