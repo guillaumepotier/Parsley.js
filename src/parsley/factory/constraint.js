4c236ecd0c2d1e3a90168a67c5fc50bb7a2db778
Marc-Andre Lafortune <github@marc-andre.ca>
2014-12-10 10:57:29 -0500

    Store created assert and reuse. This makes it clearer when asserts need to be created and is more efficient.

--- src/parsley/factory/constraint.js
+++ src/parsley/factory/constraint.js
@@ -2,28 +2,33 @@ define('parsley/factory/constraint', [
   'parsley/utils'
 ], function (ParsleyUtils) {
   var ConstraintFactory = function (parsleyField, name, requirements, priority, isDomConstraint) {
+    var assert = {};
 
     if (!new RegExp('ParsleyField').test(ParsleyUtils.get(parsleyField, '__class__')))
       throw new Error('ParsleyField or ParsleyFieldMultiple instance expected');
 
-    if ('function' !== typeof window.ParsleyValidator.validators[name] ||
-      'Assert' !== window.ParsleyValidator.validators[name](requirements).__parentClass__)
+    if ('function' === typeof window.ParsleyValidator.validators[name])
+      assert = window.ParsleyValidator.validators[name](requirements);
+    if ('Assert' !== assert.__parentClass__)
       throw new Error('Valid validator expected');
 
     var getPriority = function () {
       if ('undefined' !== typeof parsleyField.options[name + 'Priority'])
         return parsleyField.options[name + 'Priority'];
 
-      return ParsleyUtils.get(window.ParsleyValidator.validators[name](requirements), 'priority') || 2;
+      return ParsleyUtils.get(assert, 'priority') || 2;
     };
 
     priority = priority || getPriority();
 
     // If validator have a requirementsTransformer, execute it
-    if ('function' === typeof window.ParsleyValidator.validators[name](requirements).requirementsTransformer)
-      requirements = window.ParsleyValidator.validators[name](requirements).requirementsTransformer();
+    if ('function' === typeof assert.requirementsTransformer) {
+      requirements = assert.requirementsTransformer();
+      // rebuild assert with new requirements
+      assert = window.ParsleyValidator.validators[name](requirements);
+    }
 
-    return $.extend(window.ParsleyValidator.validators[name](requirements), {
+    return $.extend(assert, {
       name: name,
       requirements: requirements,
       priority: priority,
