2959688ad1dd4645204ddbd2643ee98bebfda9c4
Marc-Andre Lafortune <github@marc-andre.ca>
2014-12-11 10:31:55 -0500

    Let's not pollute

--- src/extra/validator/words.js
+++ src/extra/validator/words.js
@@ -1,5 +1,6 @@
+(function () {
 // minwords, maxwords, words extra validators
-window._countWords = function (string) {
+var countWords = function (string) {
   return string
       .replace( /(^\s*)|(\s*$)/gi, "" )
       .replace( /[ ]{2,}/gi, " " )
@@ -10,21 +11,22 @@ window._countWords = function (string) {
 ParsleyValidator.addValidator(
 	'minwords',
 	function (value, nbWords) {
-		return window._countWords(value) >= nbWords;
+		return countWords(value) >= nbWords;
 	}, 32)
 	.addMessage('en', 'minwords', 'This value needs more words');
 
 ParsleyValidator.addValidator(
 	'maxwords',
 	function (value, nbWords) {
-		return window._countWords(value) <= nbWords;
+		return countWords(value) <= nbWords;
 	}, 32)
 	.addMessage('en', 'maxwords', 'This value needs fewer words');
 
 ParsleyValidator.addValidator(
 	'words',
 	function (value, arrayRange) {
-		var length = window._countWords(value);
+		var length = countWords(value);
 		return length >= arrayRange[0] && length <= arrayRange[1];
 	}, 32)
 	.addMessage('en', 'words', 'This value has the incorrect number of words');
+})();
