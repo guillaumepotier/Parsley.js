2af6b0bf583ea639eb753ae853f1261be25f2e0a
Marc-Andre Lafortune <github@marc-andre.ca>
2014-12-11 10:06:09 -0500

    Modify comparisons in extra to accept direct numbers [#785]

--- test/features/extra.js
+++ test/features/extra.js
@@ -20,6 +20,7 @@ define('features/extra', [
         expect(window.ParsleyValidator.validators).to.have.key('gt');
         var number = 5;
 
+        // Check with a selector
         $('body').append('<input type="text" id="element" data-parsley-gt="#gt" required /><input type="text" id="gt" value="' + number + '" />');
         expect($('#element').psly().isValid()).to.be(false);
         $('#element').val(number + 1);
@@ -28,12 +29,24 @@ define('features/extra', [
         expect($('#element').psly().isValid()).to.be(false);
         $('#element').val(number - 1);
         expect($('#element').psly().isValid()).to.be(false);
+
+        // Check with a (different) number
+        number = 42;
+        $('#element').attr('data-parsley-gt', number);
+        $('#element').val(number + 1);
+        expect($('#element').psly().isValid()).to.be(true);
+        $('#element').val(number);
+        expect($('#element').psly().isValid()).to.be(false);
+        $('#element').val(number - 1);
+        expect($('#element').psly().isValid()).to.be(false);
+
         $('#gt').remove();
       });
       it('should have gte validator', function () {
         expect(window.ParsleyValidator.validators).to.have.key('gte');
         var number = 5;
 
+        // Check with a selector
         $('body').append('<input type="text" id="element" data-parsley-gte="#gte" required /><input type="text" id="gte" value="' + number + '" />');
         expect($('#element').psly().isValid()).to.be(false);
         $('#element').val(number + 1);
@@ -42,12 +55,24 @@ define('features/extra', [
         expect($('#element').psly().isValid()).to.be(true);
         $('#element').val(number - 1);
         expect($('#element').psly().isValid()).to.be(false);
+
+        // Check with a (different) number
+        number = 42;
+        $('#element').attr('data-parsley-gte', number);
+        $('#element').val(number + 1);
+        expect($('#element').psly().isValid()).to.be(true);
+        $('#element').val(number);
+        expect($('#element').psly().isValid()).to.be(true);
+        $('#element').val(number - 1);
+        expect($('#element').psly().isValid()).to.be(false);
+
         $('#gte').remove();
       });
       it('should have lt validator', function () {
         expect(window.ParsleyValidator.validators).to.have.key('lt');
         var number = 5;
 
+        // Check with a selector
         $('body').append('<input type="text" id="element" data-parsley-lt="#lt" required /><input type="text" id="lt" value="' + number + '" />');
         expect($('#element').psly().isValid()).to.be(false);
         $('#element').val(number + 1);
@@ -56,12 +81,24 @@ define('features/extra', [
         expect($('#element').psly().isValid()).to.be(false);
         $('#element').val(number - 1);
         expect($('#element').psly().isValid()).to.be(true);
+
+        // Check with a (different) number
+        number = 42;
+        $('#element').attr('data-parsley-lt', number);
+        $('#element').val(number + 1);
+        expect($('#element').psly().isValid()).to.be(false);
+        $('#element').val(number);
+        expect($('#element').psly().isValid()).to.be(false);
+        $('#element').val(number - 1);
+        expect($('#element').psly().isValid()).to.be(true);
+
         $('#lt').remove();
       });
       it('should have lte validator', function () {
         expect(window.ParsleyValidator.validators).to.have.key('lte');
         var number = 5;
 
+        // Check with a selector
         $('body').append('<input type="text" id="element" data-parsley-lte="#lte" required /><input type="text" id="lte" value="' + number + '" />');
         expect($('#element').psly().isValid()).to.be(false);
         $('#element').val(number + 1);
@@ -70,6 +107,17 @@ define('features/extra', [
         expect($('#element').psly().isValid()).to.be(true);
         $('#element').val(number - 1);
         expect($('#element').psly().isValid()).to.be(true);
+
+        // Check with a (different) number
+        number = 42;
+        $('#element').attr('data-parsley-lte', number);
+        $('#element').val(number + 1);
+        expect($('#element').psly().isValid()).to.be(false);
+        $('#element').val(number);
+        expect($('#element').psly().isValid()).to.be(true);
+        $('#element').val(number - 1);
+        expect($('#element').psly().isValid()).to.be(true);
+
         $('#lte').remove();
       });
       it('should have a minwords validator', function () {
