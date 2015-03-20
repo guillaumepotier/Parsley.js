18c93e3e4c343282cddaf11d474c8181370f4846
Alexander Shepetko <a@shepetko.com>
2015-03-20 08:08:12 +0200

    Ukrainian language support

---
+++ src/i18n/uk.extra.js
@@ -0,0 +1,9 @@
+window.ParsleyConfig = window.ParsleyConfig || {};
+window.ParsleyConfig.i18n = window.ParsleyConfig.i18n || {};
+
+window.ParsleyConfig.i18n.uk = jQuery.extend(window.ParsleyConfig.i18n.uk || {}, {
+  dateiso:  "Це значення має бути коректною датою (РРРР-ММ-ДД).",
+  minwords: "Це значення повинно містити не менше %s слів.",
+  maxwords: "Це значення повинно містити не більше %s слів.",
+  words:    "Це значення повинно містити від %s до %s слів."
+});
