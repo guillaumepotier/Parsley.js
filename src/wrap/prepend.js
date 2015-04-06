96bf2dd2d0f6e85335aa0640566482178fd8f701
Marc-Andre Lafortune <github@marc-andre.ca>
2015-04-06 11:35:51 -0400

    Be UMD compatible [#708]

--- src/wrap/prepend.js
+++ src/wrap/prepend.js
@@ -9,8 +9,11 @@
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module depending on jQuery.
     define(['jquery'], factory);
+  } else if (typeof exports === 'object') {
+    // Node/CommonJS
+    module.exports = factory(require('jquery'));
   } else {
-    // No AMD. Register plugin with global jQuery object.
+    // Register plugin with global jQuery object.
     factory(jQuery);
   }
 }(function ($) {
