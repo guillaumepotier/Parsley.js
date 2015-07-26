ebb1ed0b11c60bcf3cc98679fb092801f714d173
Marc-Andre Lafortune <github@marc-andre.ca>
2015-07-26 01:34:03 +0100

    Add extra requirement parameters

--- test/features/validator.js
+++ test/features/validator.js
@@ -1,10 +1,10 @@
 define(function () {
   return function (Validator) {
     describe('Validator', function () {
-      var testParsing = function(type, input, output) {
+      var testParsing = function(type, input, output, extraOptions) {
         it('parses '+type+' requirements', function () {
           var c = new Validator({requirementType: type});
-          expect(c.parseRequirements(input)).to.eql(output);
+          expect(c.parseRequirements(input, extraOptions)).to.eql(output);
         });
       }
 
@@ -12,6 +12,14 @@ define(function () {
       testParsing('number', '4.2', [4.2]);
       testParsing('string', '42', ['42']);
       testParsing(['number', 'string'], '[4.2, 4.2]', [4.2, '4.2']);
+      testParsing({
+          '': 'number',
+          'foo': 'string',
+          'bar': 'string'
+        }, '4.2',
+        [4.2, {foo: 'FOO', bar: 'BAR'}],
+        function(value) { return value.toUpperCase(); }
+      );
     });
   };
 });
