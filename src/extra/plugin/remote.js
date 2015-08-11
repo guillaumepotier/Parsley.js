6708cae0be6195686d6030e25f8812af8fee440d
Guillaume Potier <cobalt2760@gmail.com>
2015-08-10 21:58:26 -0400

    Indent code

--- src/extra/plugin/remote.js
+++ src/extra/plugin/remote.js
@@ -1,104 +1,104 @@
 (function ($) {
 
-$.extend(true, window.Parsley, {
-  asyncValidators: {
-    default: {
-      fn: function (xhr) {
-        return 'resolved' === xhr.state();
+  $.extend(true, window.Parsley, {
+    asyncValidators: {
+      default: {
+        fn: function (xhr) {
+          return 'resolved' === xhr.state();
+        },
+        url: false
       },
-      url: false
+      reverse: {
+        fn: function (xhr) {
+          // If reverse option is set, a failing ajax request is considered successful
+          return 'rejected' === xhr.state();
+        },
+        url: false
+      }
     },
-    reverse: {
-      fn: function (xhr) {
-        // If reverse option is set, a failing ajax request is considered successful
-        return 'rejected' === xhr.state();
-      },
-      url: false
+
+    addAsyncValidator: function (name, fn, url, options) {
+      window.Parsley.asyncValidators[name.toLowerCase()] = {
+        fn: fn,
+        url: url || false,
+        options: options || {}
+      };
+
+      return this;
+    },
+
+    eventValidate: function (event) {
+      // For keyup, keypress, keydown.. events that could be a little bit obstrusive
+      // do not validate if val length < min threshold on first validation. Once field have been validated once and info
+      // about success or failure have been displayed, always validate with this trigger to reflect every yalidation change.
+      if (new RegExp('key').test(event.type))
+        if (!this._ui.validationInformationVisible && this.getValue().length <= this.options.validationThreshold)
+          return;
+
+      this._ui.validatedOnce = true;
+      this.whenValidate();
     }
-  },
+  });
 
-  addAsyncValidator: function (name, fn, url, options) {
-    window.Parsley.asyncValidators[name.toLowerCase()] = {
-      fn: fn,
-      url: url || false,
-      options: options || {}
-    };
+  window.ParsleyValidator.addValidator('remote', {
+    requirementType: {
+      '': 'string',
+      'validator': 'string',
+      'reverse': 'boolean',
+      'options': 'object'
+    },
 
-    return this;
-  },
+    validateString: function (value, url, options, instance) {
+      var
+        csr,
+        data = {},
+        ajaxOptions,
+        validator = options.validator || (true === options.reverse ? 'reverse' : 'default');
 
-  eventValidate: function (event) {
-    // For keyup, keypress, keydown.. events that could be a little bit obstrusive
-    // do not validate if val length < min threshold on first validation. Once field have been validated once and info
-    // about success or failure have been displayed, always validate with this trigger to reflect every yalidation change.
-    if (new RegExp('key').test(event.type))
-      if (!this._ui.validationInformationVisible && this.getValue().length <= this.options.validationThreshold)
-        return;
+      if ('undefined' === typeof window.Parsley.asyncValidators[validator])
+        throw new Error('Calling an undefined async validator: `' + validator + '`');
 
-    this._ui.validatedOnce = true;
-    this.whenValidate();
-  }
-});
+      // Fill data with current value
+      data[instance.$element.attr('name') || instance.$element.attr('id')] = value;
 
-window.ParsleyValidator.addValidator('remote', {
-  requirementType: {
-    '': 'string',
-    'validator': 'string',
-    'reverse': 'boolean',
-    'options': 'object'
-  },
+      // Merge options passed in from the function with the ones in the attribute
+      var remoteOptions = $.extend(true, options.options || {} , window.Parsley.asyncValidators[validator].options);
 
-  validateString: function (value, url, options, instance) {
-    var
-      csr,
-      data = {},
-      ajaxOptions,
-      validator = options.validator || (true === options.reverse ? 'reverse' : 'default');
+      // All `$.ajax(options)` could be overridden or extended directly from DOM in `data-parsley-remote-options`
+      ajaxOptions = $.extend(true, {}, {
+        url: window.Parsley.asyncValidators[validator].url || url,
+        data: data,
+        type: 'GET'
+      }, remoteOptions);
 
-    if ('undefined' === typeof window.Parsley.asyncValidators[validator])
-      throw new Error('Calling an undefined async validator: `' + validator + '`');
+      // Generate store key based on ajax options
+      csr = $.param(ajaxOptions);
 
-    // Fill data with current value
-    data[instance.$element.attr('name') || instance.$element.attr('id')] = value;
+      // Initialise querry cache
+      if ('undefined' === typeof window.Parsley._remoteCache)
+        window.Parsley._remoteCache = {};
 
-    // Merge options passed in from the function with the ones in the attribute
-    var remoteOptions = $.extend(true, options.options || {} , window.Parsley.asyncValidators[validator].options);
+      // Try to retrieve stored xhr
+      var xhr = window.Parsley._remoteCache[csr] = window.Parsley._remoteCache[csr] || $.ajax(ajaxOptions);
 
-    // All `$.ajax(options)` could be overridden or extended directly from DOM in `data-parsley-remote-options`
-    ajaxOptions = $.extend(true, {}, {
-      url: window.Parsley.asyncValidators[validator].url || url,
-      data: data,
-      type: 'GET'
-    }, remoteOptions);
+      var handleXhr = function () {
+        var result = window.Parsley.asyncValidators[validator].fn.call(instance, xhr, url, options);
 
-    // Generate store key based on ajax options
-    csr = $.param(ajaxOptions);
+        // Map falsy results to rejected promise
+        if (!result)
+          result = $.Deferred().reject();
 
-    // Initialise querry cache
-    if ('undefined' === typeof window.Parsley._remoteCache)
-      window.Parsley._remoteCache = {};
+        return $.when(result);
+      };
 
-    // Try to retrieve stored xhr
-    var xhr = window.Parsley._remoteCache[csr] = window.Parsley._remoteCache[csr] || $.ajax(ajaxOptions);
+      return xhr.then(handleXhr, handleXhr);
+    },
 
-    var handleXhr = function () {
-      var result = window.Parsley.asyncValidators[validator].fn.call(instance, xhr, url, options);
+    priority: -1
+  });
 
-      // Map falsy results to rejected promise
-      if (!result)
-        result = $.Deferred().reject();
-
-      return $.when(result);
-    };
-
-    return xhr.then(handleXhr, handleXhr);
-  },
-
-  priority: -1
-});
-
-window.Parsley.on('form:submit', function () {
-  window.Parsley._remoteCache = {};
-});
+  window.Parsley.on('form:submit', function () {
+    window.Parsley._remoteCache = {};
+  });
 
 })(jQuery);
