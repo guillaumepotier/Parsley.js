7d4c2ff94fdde95b5891ffff2210f7553fa2d37f
Marc-Andre Lafortune <github@marc-andre.ca>
2015-10-20 16:20:01 -0400

    Indent

--- src/extra/plugin/bind.js
+++ src/extra/plugin/bind.js
@@ -4,47 +4,47 @@
 
 import jQuery from 'jquery'; // Remove this line in ES3
 
-(function($){
+(function ($) {
 
-window.ParsleyConfig = $.extend(true, window.ParsleyConfig, { autoBind: false });
-window.ParsleyExtend = window.ParsleyExtend || {};
+  window.ParsleyConfig = $.extend(true, window.ParsleyConfig, { autoBind: false });
+  window.ParsleyExtend = window.ParsleyExtend || {};
 
-window.ParsleyExtend = $.extend(window.ParsleyExtend, {
-  // { '#selector' : { constraintName1: value, constraintName2: value2 }, #selector2: { constraintName: value } }
-  // { '#selector' : { constraintName1: { requirements: value, priority: value }, constraintName2: value2 } }
-  _bindFields: function () {
-    if ('ParsleyForm' !== this.__class__)
-      throw new Error('`_bindFields` must be called on a form instance');
+  window.ParsleyExtend = $.extend(window.ParsleyExtend, {
+    // { '#selector' : { constraintName1: value, constraintName2: value2 }, #selector2: { constraintName: value } }
+    // { '#selector' : { constraintName1: { requirements: value, priority: value }, constraintName2: value2 } }
+    _bindFields: function () {
+      if ('ParsleyForm' !== this.__class__)
+        throw new Error('`_bindFields` must be called on a form instance');
 
-    if ('undefined' === typeof this.options.fields)
-      throw new Error('bind.js plugin needs to have Parsley instantiated with fields');
+      if ('undefined' === typeof this.options.fields)
+        throw new Error('bind.js plugin needs to have Parsley instantiated with fields');
 
-    var field;
-    this.fields = [];
+      var field;
+      this.fields = [];
 
-    for (var selector in this.options.fields) {
-      if (0 === $(selector).length)
-        continue;
+      for (var selector in this.options.fields) {
+        if (0 === $(selector).length)
+          continue;
 
-      field = $(selector).parsley();
+        field = $(selector).parsley();
 
-      for (var name in this.options.fields[selector]) {
-        if ('object' === typeof this.options.fields[selector][name] && !(this.options.fields[selector][name] instanceof Array))
-          field.addConstraint(name.toLowerCase(), this.options.fields[selector][name].requirements, this.options.fields[selector][name].priority || 32);
-        else
-          field.addConstraint(name.toLowerCase(), this.options.fields[selector][name]);
+        for (var name in this.options.fields[selector]) {
+          if ('object' === typeof this.options.fields[selector][name] && !(this.options.fields[selector][name] instanceof Array))
+            field.addConstraint(name.toLowerCase(), this.options.fields[selector][name].requirements, this.options.fields[selector][name].priority || 32);
+          else
+            field.addConstraint(name.toLowerCase(), this.options.fields[selector][name]);
+        }
       }
+
+      this.fields.push(field);
+
+      return this;
+    },
+
+    // Do nothing
+    _bindConstraints: function () {
+      return this;
     }
-
-    this.fields.push(field);
-
-    return this;
-  },
-
-  // Do nothing
-  _bindConstraints: function () {
-    return this;
-  }
-});
+  });
 
 })(jQuery);
