30bd60c098b0eb8a2f75fe0f74cbdf718dcbb166
Marc-Andre Lafortune <github@marc-andre.ca>
2015-10-20 19:53:15 -0400

    Fix whitespace

--- src/extra/validator/words.js
+++ src/extra/validator/words.js
@@ -8,24 +8,24 @@ var countWords = function (string) {
 };
 
 window.Parsley.addValidator(
-	'minwords',
-	function (value, nbWords) {
-		return countWords(value) >= nbWords;
-	}, 32)
-	.addMessage('en', 'minwords', 'This value needs more words');
+  'minwords',
+  function (value, nbWords) {
+    return countWords(value) >= nbWords;
+  }, 32)
+  .addMessage('en', 'minwords', 'This value needs more words');
 
 window.Parsley.addValidator(
-	'maxwords',
-	function (value, nbWords) {
-		return countWords(value) <= nbWords;
-	}, 32)
-	.addMessage('en', 'maxwords', 'This value needs fewer words');
+  'maxwords',
+  function (value, nbWords) {
+    return countWords(value) <= nbWords;
+  }, 32)
+  .addMessage('en', 'maxwords', 'This value needs fewer words');
 
 window.Parsley.addValidator(
-	'words',
-	function (value, arrayRange) {
-		var length = countWords(value);
-		return length >= arrayRange[0] && length <= arrayRange[1];
-	}, 32)
-	.addMessage('en', 'words', 'This value has the incorrect number of words');
+  'words',
+  function (value, arrayRange) {
+    var length = countWords(value);
+    return length >= arrayRange[0] && length <= arrayRange[1];
+  }, 32)
+  .addMessage('en', 'words', 'This value has the incorrect number of words');
 })();
