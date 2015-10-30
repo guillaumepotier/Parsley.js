0c46c41780997e666a6864207c8a803dcf69b163
Marc-Andre Lafortune <github@marc-andre.ca>
2015-10-30 18:43:39 -0400

    Add 'Play in CodePen' button to simple example

--- doc/assets/example.js
+++ doc/assets/example.js
@@ -7,6 +7,35 @@
     js +
     '\n<' + '/script>\n'
   );
+
+  $('.play').click(function() {
+    var css = $.trim($('style.example').text());
+
+    var data = {
+      title: $(document).attr('title'),
+      description: "Where does this show???",
+      html: html,
+      js: js,
+      css: css + '\nhtml.codepen body {\n  margin: 1em;\n}\n',
+      html_classes: 'codepen',
+      js_external: [
+        '//code.jquery.com/jquery-2.1.3.js',
+        'https://rawgit.com/guillaumepotier/Parsley.js/2.2.0-rc4/dist/parsley.js'
+      ].join(';'),
+      css_external: [
+        'https://rawgit.com/guillaumepotier/Parsley.js/2.2.0-rc4/bower_components/bootstrap/dist/css/bootstrap.css',
+        'https://rawgit.com/guillaumepotier/Parsley.js/2.2.0-rc4/doc/assets/docs.css',
+        'https://rawgit.com/guillaumepotier/Parsley.js/2.2.0-rc4/src/parsley.css'
+      ].join(';')
+    };
+    console.log(data);
+    var $input = $('<input type="hidden" name="data">')
+      .val(JSON.stringify(data));
+    var $form = $('<form action="http://codepen.io/pen/define?editors=101" method="POST" target="_blank">')
+      .append($input)
+      .submit();
+  })
+
   try {
     hljs.initHighlightingOnLoad();
   } catch ( err ) {}
