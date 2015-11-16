57d495925cc5f7a3a48c93c2a5a667de6864ee5d
Jasper Lammens <jasper.lammens@intracto.com>
2015-11-16 09:21:49 +0100

    Set french locale after adding the messages

--- src/i18n/fr.extra.js
+++ src/i18n/fr.extra.js
@@ -1,5 +1,5 @@
 // Validation errors messages for Parsley
-import Parsley from 'parsley';
+import Parsley from '../parsley';
 
 Parsley.addMessages('fr', {
   dateiso: "Cette valeur n'est pas une date valide (YYYY-MM-DD).",
