babaede2c50cc2ac4abf983a1661b4dbf0b41d52
Marc-Andre Lafortune <github@marc-andre.ca>
2016-04-13 14:07:59 -0400

    Update inputevent to check type=file also

--- src/vendor/inputevent.js
+++ src/vendor/inputevent.js
@@ -1,7 +1,7 @@
 /**
  * inputevent - Alleviate browser bugs for input events
  * https://github.com/marcandre/inputevent
- * @version v0.0.1 - (built Tue, Apr 12th 2016, 4:31 pm)
+ * @version v0.0.2 - (built Wed, Apr 13th 2016, 2:06 pm)
  * @author Marc-Andre Lafortune <github@marc-andre.ca>
  * @license MIT
  */
@@ -11,11 +11,11 @@ import $ from 'jquery';
 function InputEvent() {
   let globals = window || global;
 
-  // Slightly odd way to have the object constructed have method force bound.
-  // Used to test duplicate library
+  // Slightly odd way construct our object. This way methods are force bound.
+  // Used to test for duplicate library.
   $.extend(this, {
 
-    inputsToCheck: ['select', 'input[type="checkbox"]', 'input[type="radio"]'],
+    inputsToCheck: ['select', 'input[type="checkbox"]', 'input[type="radio"]', 'input[type="file"]'],
 
     // For browsers that do not support isTrusted, assumes event is native.
     isNativeEvent: evt => {
@@ -50,7 +50,7 @@ function InputEvent() {
       if (globals.inputEventPatched) {
         return;
       }
-      globals.inputEventPatched = '0.0.1';
+      globals.inputEventPatched = '0.0.2';
       for (let selector of this.inputsToCheck) {
         $(document)
           .on('input.inputevent', selector, {selector}, this.behavesOk)
