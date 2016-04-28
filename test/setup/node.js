fc2763824a2faff37a253bed2717a2c4fe234047
Marc-Andre Lafortune <github@marc-andre.ca>
2016-04-28 14:58:31 -0400

    Ditch outdated simple-jsdom, use jsdom directly [#1095]

--- test/setup/node.js
+++ test/setup/node.js
@@ -4,8 +4,19 @@ global.travis = process.env.TRAVIS;
 
 require('babel-core/register');
 
-import simpleJSDom from 'simple-jsdom';
-simpleJSDom.install();
+
+var jsdom = require('jsdom').jsdom;
+var document = jsdom('<html><head></head><body></body></html>');
+var window = document.defaultView;
+var navigator = window.navigator = {
+  userAgent: 'NodeJS JSDom',
+  appVersion: ''
+};
+
+global.jsdom = jsdom;
+global.document = document;
+global.window = window;
+global.navigator = navigator;
 
 require('./setup')();
 
