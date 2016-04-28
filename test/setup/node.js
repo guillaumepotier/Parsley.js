2095a002397d6637184d787bd82a94f44fa5b9e3
Marc-Andre Lafortune <github@marc-andre.ca>
2016-04-28 15:19:08 -0400

    Simplify

--- test/setup/node.js
+++ test/setup/node.js
@@ -4,19 +4,13 @@ global.travis = process.env.TRAVIS;
 
 require('babel-core/register');
 
-
 var jsdom = require('jsdom').jsdom;
-var document = jsdom('<html><head></head><body></body></html>');
-var window = document.defaultView;
-var navigator = window.navigator = {
+global.document = jsdom('<html><head></head><body></body></html>');
+global.window = document.defaultView;
+global.navigator = window.navigator = {
   userAgent: 'NodeJS JSDom',
   appVersion: ''
 };
-
-global.jsdom = jsdom;
-global.document = document;
-global.window = window;
-global.navigator = navigator;
 
 require('./setup')();
 
