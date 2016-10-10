af00018aa132d6a66364c99e1cfa01ef65ae81a4
Thomas Portelange <thomas@lekoala.be>
2016-10-10 12:31:19 +0200

    exchange min and max

--- src/i18n/fr.extra.js
+++ src/i18n/fr.extra.js
@@ -3,8 +3,8 @@ import Parsley from '../parsley';
 
 Parsley.addMessages('fr', {
   dateiso:    "Cette valeur n'est pas une date valide (YYYY-MM-DD).",
-  minwords:   "Cette valeur est trop courte. Elle doit contenir tout au plus %s mots.",
-  maxwords:   "Cette valeur est trop longue. Elle doit contenir au moins %s mots.",
+  minwords:   "Cette valeur est trop courte. Elle doit contenir au moins %s mots.",
+  maxwords:   "Cette valeur est trop longue. Elle doit contenir tout au plus %s mots.",
   words:      "Cette valeur est invalide. Elle doit contenir entre %s et %s mots.",
   gt:         "Cette valeur doit être plus grande.",
   gte:        "Cette valeur doit être plus grande ou égale.",
