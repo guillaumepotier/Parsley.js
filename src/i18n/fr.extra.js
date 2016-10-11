3176d0f167fdb961c53e7fd66e1b935c11796a7f
Thomas Portelange <thomas@lekoala.be>
2016-10-11 04:20:15 +0200

    Update fr extra translations (#1136)
    * Update fr extra translations

--- src/i18n/fr.extra.js
+++ src/i18n/fr.extra.js
@@ -2,6 +2,13 @@
 import Parsley from '../parsley';
 
 Parsley.addMessages('fr', {
-  dateiso: "Cette valeur n'est pas une date valide (YYYY-MM-DD).",
+  dateiso:    "Cette valeur n'est pas une date valide (YYYY-MM-DD).",
+  minwords:   "Cette valeur est trop courte. Elle doit contenir au moins %s mots.",
+  maxwords:   "Cette valeur est trop longue. Elle doit contenir tout au plus %s mots.",
+  words:      "Cette valeur est invalide. Elle doit contenir entre %s et %s mots.",
+  gt:         "Cette valeur doit être plus grande.",
+  gte:        "Cette valeur doit être plus grande ou égale.",
+  lt:         "Cette valeur doit être plus petite.",
+  lte:        "Cette valeur doit être plus petite ou égale.",
   notequalto: "Cette valeur doit être différente."
 });
