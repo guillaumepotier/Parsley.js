2ec75e49221eefb1dc61311befe54de5a959d6a9
Marc-Andre Lafortune <github@marc-andre.ca>
2017-01-27 23:59:38 +0100

    Tweak whitespace

--- src/i18n/sr.extra.js
+++ src/i18n/sr.extra.js
@@ -2,13 +2,13 @@
 import Parsley from '../parsley';
 
 Parsley.addMessages('sr', {
-	dateiso:  "Unesite validan datum u formatu YYYY-MM-DD.",
-	minwords: "Potrebno je da unesete %s ili više reči.",
-	maxwords: "Moguće je uneti maksimalno %s reči.",
-	words:    "Potrebno je da unesete između %s i %s reči.",
-	gt:       "Ova vrednost mora da bude veća.",
-	gte:      "Ova vrednost mora da bude veća ili jednaka.",
-	lt:       "Ova vrednost mora da bude manja.",
-	lte:      "Ova vrednost mora da bude manja ili jednaka.",
-	notequalto: "Sadržaj ovog polja mora biti različit."
+  dateiso:  "Unesite validan datum u formatu YYYY-MM-DD.",
+  minwords: "Potrebno je da unesete %s ili više reči.",
+  maxwords: "Moguće je uneti maksimalno %s reči.",
+  words:    "Potrebno je da unesete između %s i %s reči.",
+  gt:       "Ova vrednost mora da bude veća.",
+  gte:      "Ova vrednost mora da bude veća ili jednaka.",
+  lt:       "Ova vrednost mora da bude manja.",
+  lte:      "Ova vrednost mora da bude manja ili jednaka.",
+  notequalto: "Sadržaj ovog polja mora biti različit."
 });
