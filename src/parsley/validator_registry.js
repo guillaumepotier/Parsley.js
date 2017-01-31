cbef75e5ec5113560a412f2abadfa01ff325b3cd
Marc-Andre Lafortune <github@marc-andre.ca>
2017-01-31 18:52:48 +0100

    Rename variables [prep for #1168]

--- src/parsley/validator_registry.js
+++ src/parsley/validator_registry.js
@@ -12,7 +12,7 @@ var ValidatorRegistry = function (validators, catalog) {
   this.init(validators || {}, catalog || {});
 };
 
-var typeRegexes =  {
+var typeTesters =  {
   email: /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i,
 
   // Follow https://www.w3.org/TR/html5/infrastructure.html#floating-point-numbers
@@ -59,7 +59,7 @@ var typeRegexes =  {
       "$", 'i'
     )
 };
-typeRegexes.range = typeRegexes.number;
+typeTesters.range = typeTesters.number;
 
 // See http://stackoverflow.com/a/10454560/8279
 var decimalPlaces = num => {
@@ -234,11 +234,11 @@ ValidatorRegistry.prototype = {
     },
     type: {
       validateString: function(value, type, {step = 'any', base = 0} = {}) {
-        var regex = typeRegexes[type];
-        if (!regex) {
+        var tester = typeTesters[type];
+        if (!tester) {
           throw new Error('validator type `' + type + '` is not supported');
         }
-        if (!regex.test(value))
+        if (!tester.test(value))
           return false;
         if ('number' === type) {
           if (!/^any$/i.test(step || '')) {
