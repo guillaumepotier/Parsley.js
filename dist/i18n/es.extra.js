3bbd4fbb1ca35b2a52bf2fa6632462704af3ae86
Pablo Garcia <pablodgcatulo@gmail.com>
2017-02-08 14:01:25 -0300

    Added spanish translations to extra validators

---
+++ dist/i18n/es.extra.js
@@ -0,0 +1,14 @@
+// Validation errors messages for Parsley
+// Load this after Parsley
+
+Parsley.addMessages('es', {
+  dateiso:  "Este valor debe ser una fecha válida (YYYY-MM-DD).",
+  minwords: "This value is too short. It should have %s words or more.",
+  maxwords: "Este valor es muy corto. La longitud máxima es de %s palabras.",,
+  words:    "Este valor es inválido. Debe tener entre %s y %s palabras de longitud.",
+  gt:       "Este valor debe ser mayor.",
+  gte:      "Este valor debe ser mayor o igual.",
+  lt:       "Este valor debe ser menor.",
+  lte:      "Este valor debe ser menor o igual.",
+  notequalto: "Este valor debe ser diferente."
+});
