39996dd2225ef0bf914f0c39afb7a8be711be3be
Marc-Andre Lafortune <github@marc-andre.ca>
2017-02-22 11:48:48 -0500

    No clue why only this format works with newer Babel

--- src/header.js
+++ src/header.js
@@ -1,6 +1,6 @@
 /*!
 * Parsley.js
-* Version ${pkg.version} - built ${now.format('ddd, MMM Do YYYY, h:mm a')}
+* Version <%= pkg.version %> - built <%= now.format('ddd, MMM Do YYYY, h:mm a') %>
 * http://parsleyjs.org
 * Guillaume Potier - <guillaume@wisembly.com>
 * Marc-Andre Lafortune - <petroselinum@marc-andre.ca>
