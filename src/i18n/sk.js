e7638806fa27f9a51f703eb9799beb1c1591b747
Adam Mikó <okukster@gmail.com>
2018-04-30 15:50:33 +0200

    Update sk.js

--- src/i18n/sk.js
+++ src/i18n/sk.js
@@ -6,23 +6,23 @@ Parsley.addMessages('sk', {
   type: {
     email:        "Prosím zadajte správnu emailovú adresu.",
     url:          "Prosím zadajte platnú URL adresu.",
-    number:       "Toto pole môže obsahovať len čísla",
-    integer:      "Toto pole môže obsahovať len celé čísla",
+    number:       "Toto pole môže obsahovať len čísla.",
+    integer:      "Toto pole môže obsahovať len celé čísla.",
     digits:       "Toto pole môže obsahovať len kladné celé čísla.",
     alphanum:     "Toto pole môže obsahovať len alfanumerické znaky."
   },
   notblank:       "Toto pole nesmie byť prázdne.",
   required:       "Toto pole je povinné.",
-  pattern:        "Toto pole je je neplatné.",
+  pattern:        "Toto pole je neplatné.",
   min:            "Prosím zadajte hodnotu väčšiu alebo rovnú %s.",
   max:            "Prosím zadajte hodnotu menšiu alebo rovnú %s.",
   range:          "Prosím zadajte hodnotu v rozmedzí %s a %s",
-  minlength:      "Prosím zadajte hodnotu dlhú %s znakov a viacej.",
+  minlength:      "Prosím zadajte hodnotu dlhšiu ako %s znakov.",
   maxlength:      "Prosím zadajte hodnotu kratšiu ako %s znakov.",
   length:         "Prosím zadajte hodnotu medzi %s a %s znakov.",
-  mincheck:       "Je nutné vybrať minimálne %s z možností.",
-  maxcheck:       "Je nutné vybrať maximálne %s z možností.",
-  check:          "Je nutné vybrať od %s do %s z možností.",
+  mincheck:       "Je nutné vybrať minimálne %s možností.",
+  maxcheck:       "Je nutné vybrať maximálne %s možností.",
+  check:          "Je nutné vybrať od %s do %s možností.",
   equalto:        "Prosím zadajte rovnakú hodnotu."
 });
 
