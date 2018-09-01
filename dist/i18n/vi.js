caf11824559ff1d9d01130e7db4db71335291097
jakubstar <42865850+jakubstar@users.noreply.github.com>
2018-09-01 09:43:00 +0200

    Vietnamese language added

---
+++ dist/i18n/vi.js
@@ -0,0 +1,26 @@
+Parsley.addMessages('vi', {
+  defaultMessage: "Giá trị này có vẻ không hợp lệ.",
+  type: {
+    email:        "Giá trị này phải là một e-mail hợp lệ.",
+    url:          "Giá trị này phải là url hợp lệ.",
+    number:       "Giá trị này phải là một con số hợp lệ.",
+    integer:      "Giá trị này phải là một số nguyên hợp lệ.",
+    digits:       "Giá trị này phải là chữ số.",
+    alphanum:     "Giá trị này phải là chữ số."
+  },
+  notblank:       "Giá trị này không được bỏ trống.",
+  required:       "Giá trị này là bắt buộc.",
+  pattern:        "Giá trị này có vẻ không hợp lệ.",
+  min:            "Giá trị này phải lớn hơn hoặc bằng %s.",
+  max:            "Giá trị này phải nhỏ hơn hoặc bằng %s.",
+  range:          "Giá trị này phải nằm giữa %s và %s.",
+  minlength:      "Giá trị này quá ngắn. Nó phải có %s ký tự trở lên.",
+  maxlength:      "Giá trị này quá dài. Nó phải có ít hơn %s ký tự.",
+  length:         "Độ dài của giá trị này không hợp lệ. Nó phải có %s đến %s ký tự.",
+  mincheck:       "Bạn phải chọn ít nhận %s lựa chọn.",
+  maxcheck:       "Bạn phải chọn %s hoặc ít hơn lựa chọn.",
+  check:          "Bạn phải chọn giữa %s và %s lựa chọn.",
+  equalto:        "Giá trị này phải giống nhau."
+});
+
+Parsley.setLocale('vi');
