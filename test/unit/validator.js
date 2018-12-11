a2fa84be4933d9aaf23518917e9d69b5e05559dd
Marc-Andre Lafortune <github@marc-andre.ca>
2018-12-11 18:43:00 -0500

    Just the test [wip]

--- test/unit/validator.js
+++ test/unit/validator.js
@@ -4,14 +4,16 @@ describe('Validator', () => {
   var testParsing = function(type, input, output, extraOptions) {
     it(`parses ${type} requirements`, () => {
       var c = new Validator({requirementType: type});
-      expect(c.parseRequirements(input, extraOptions)).to.eql(output);
+      expect(c.parseRequirements(input, extraOptions)).to.equal(output);
     });
   };
 
   testParsing('integer', '42', [42]);
   testParsing('number', '4.2', [4.2]);
   testParsing('string', '42', ['42']);
+  testParsing('string', 42, [42]);
   testParsing(['number', 'string'], '[4.2, 4.2]', [4.2, '4.2']);
+  testParsing(['number', 'string'], [4.2, 4.2], [4.2, '4.2']);
   testParsing({
       '': 'number',
       'foo': 'string',
