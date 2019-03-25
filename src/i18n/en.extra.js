5aae2ea0edc96f67a23604a08c7a85cea8b571c7
MarcoSulla <github@marco.sulla.e4ward.com>
2019-03-26 00:30:54 +0100

    Update en.extra.js

--- src/i18n/en.extra.js
+++ src/i18n/en.extra.js
@@ -6,9 +6,9 @@ Parsley.addMessages('en', {
   minwords: "This value is too short. It should have %s words or more.",
   maxwords: "This value is too long. It should have %s words or fewer.",
   words:    "This value length is invalid. It should be between %s and %s words long.",
-  gt:       "This value should be greater.",
-  gte:      "This value should be greater or equal.",
-  lt:       "This value should be less.",
-  lte:      "This value should be less or equal.",
-  notequalto: "This value should be different."
+  gt:       "This value should be greater of %s.",
+  gte:      "This value should be greater or equal of %s.",
+  lt:       "This value should be less of %s.",
+  lte:      "This value should be less or equal of %s.",
+  notequalto: "This value should be different of %s."
 });
