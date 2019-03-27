8c8b87388fd4d0ea4e2e5f2d5d95c21fc9cd1b08
MarcoSulla <github@marco.sulla.e4ward.com>
2019-03-27 19:58:45 +0100

    Update it.extra.js

--- src/i18n/it.extra.js
+++ src/i18n/it.extra.js
@@ -2,5 +2,8 @@
 import Parsley from '../parsley';
 
 Parsley.addMessages('it', {
-  dateiso: "Inserire una data valida (AAAA-MM-GG)."
+  dateiso: "Inserire una data valida (AAAA-MM-GG).",
+  date: "Deve essere una data valida nel formato %s",
+  datebeforenow: "La data deve precedere la data attuale",
+  dateafternow: "La data deve esse posteriore alla data attuale",
 });
