dd0a8a257e509080755990d6784ccbb863f12165
Marco Sulla <github@marco.sulla.e4ward.com>
2019-05-04 21:08:22 +0200

    Update en.extra.js

--- src/i18n/en.extra.js
+++ src/i18n/en.extra.js
@@ -3,6 +3,9 @@ import Parsley from '../parsley';
 
 Parsley.addMessages('en', {
   dateiso:  "This value should be a valid date (YYYY-MM-DD).",
+  date: "It must be a valid date with format %s",
+  datebeforenow: "Date must be valid with format %s and be before now",
+  dateafternow: "Date must be valid with format %s and be after now",
   minwords: "This value is too short. It should have %s words or more.",
   maxwords: "This value is too long. It should have %s words or fewer.",
   words:    "This value length is invalid. It should be between %s and %s words long.",
