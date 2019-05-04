74a7b995f597e240598a612032eaf50b17f5bb48
Marco Sulla <github@marco.sulla.e4ward.com>
2019-05-04 21:08:22 +0200

    Add date validators to `extras/`

--- src/i18n/it.extra.js
+++ src/i18n/it.extra.js
@@ -2,7 +2,10 @@
 import Parsley from '../parsley';
 
 Parsley.addMessages('it', {
-  dateiso:    "Inserire una data valida (AAAA-MM-GG).",
+  dateiso: "Inserire una data valida (AAAA-MM-GG).",
+  date: "Deve essere una data valida nel formato %s",
+  datebeforenow: "La data deve essere valida nel formato %s e precedere la data attuale",
+  dateafternow: "La data deve essere valida nel formato %s e posteriore alla data attuale",
   minwords:   "Questo valore deve avere almeno %s parole.",
   maxwords:   "Questo valore deve avere meno di %s parole.",
   words:      "Questo valore deve avere tra %s e %s parole.",
@@ -10,5 +13,5 @@ Parsley.addMessages('it', {
   gte:        "Questo valore deve essere maggiore o uguale a %s.",
   lt:         "Questo valore deve essere minore di %s.",
   lte:        "Questo valore deve essere minore o uguale a %s.",
-  notequalto: "Questo valore deve essere differente da %s.",
+  notequalto: "Questo valore deve essere differente da %s."
 });
