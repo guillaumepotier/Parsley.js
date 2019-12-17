69c01d05c3dc429cf15b3ea1918d24235f13ca29
Edwin van de Pol <edwinvdpol@users.noreply.github.com>
2019-12-17 01:23:20 +0100

    Update NL extra translation
    Added additional translation

--- dist/i18n/nl.extra.js
+++ dist/i18n/nl.extra.js
@@ -3,9 +3,15 @@
 
 Parsley.addMessages('nl', {
   dateiso:  "Deze waarde moet een datum in het volgende formaat zijn: (YYYY-MM-DD).",
+  date:     "Dit moet een geldige datum zijn in formaat %s.",
+  datebeforenow: "Datum moet geldig zijn in het formaat %s en eerder zijn dan nu.",
+  dateafternow: "Datum moet geldig zijn in het formaat %s en later zijn dan nu.",
   minwords: "Deze waarde moet minstens %s woorden bevatten.",
   maxwords: "Deze waarde mag maximaal %s woorden bevatten.",
   words:    "Deze waarde moet tussen de %s en %s woorden bevatten.",
   gt:       "Deze waarde moet groter dan %s zijn.",
-  lt:       "Deze waarde moet kleiner dan %s zijn."
+  gte:      "Deze waarde moet groter zijn dan of gelijk zijn aan %s.",
+  lt:       "Deze waarde moet kleiner dan %s zijn.",
+  lte:      "Deze waarde moet kleiner zijn dan of gelijk zijn aan %s.",
+  notequalto: "Deze waarde moet verschillen van %s."
 });
