6ca620ab44276342a6faacd5d6b398dbbc428a4f
Maskumal <41483685+Mammo32@users.noreply.github.com>
2020-11-29 00:08:15 +0700

    Update id.js

--- dist/i18n/id.js
+++ dist/i18n/id.js
@@ -4,26 +4,26 @@
 Parsley.addMessages('id', {
   defaultMessage: "tidak valid",
   type: {
-    email:        "email tidak valid",
-    url:          "url tidak valid",
-    number:       "nomor tidak valid",
-    integer:      "integer tidak valid",
-    digits:       "harus berupa digit",
-    alphanum:     "harus berupa alphanumeric"
+    email:        "Alamat email tidak valid",
+    url:          "Url tidak valid",
+    number:       "Nomor tidak valid",
+    integer:      "Integer tidak valid",
+    digits:       "Input harus berupa digit",
+    alphanum:     "Input harus berupa alphanumeric"
   },
-  notblank:       "tidak boleh kosong",
-  required:       "tidak boleh kosong",
-  pattern:        "tidak valid",
+  notblank:       "Kolom ini harus diisi",
+  required:       "Kolom ini harus diisi",
+  pattern:        "Input tidak valid",
   min:            "harus lebih besar atau sama dengan %s.",
   max:            "harus lebih kecil atau sama dengan %s.",
   range:          "harus dalam rentang %s dan %s.",
-  minlength:      "terlalu pendek, minimal %s karakter atau lebih.",
-  maxlength:      "terlalu panjang, maksimal %s karakter atau kurang.",
-  length:         "panjang karakter harus dalam rentang %s dan %s",
-  mincheck:       "pilih minimal %s pilihan",
-  maxcheck:       "pilih maksimal %s pilihan",
-  check:          "pilih antar %s dan %s pilihan",
-  equalto:        "harus sama"
+  minlength:      "Input Terlalu pendek, minimal %s karakter atau lebih.",
+  maxlength:      "Input terlalu panjang, maksimal %s karakter atau kurang.",
+  length:         "Panjang karakter harus dalam rentang %s dan %s",
+  mincheck:       "Pilih minimal %s pilihan",
+  maxcheck:       "Pilih maksimal %s pilihan",
+  check:          "Pilih antar %s dan %s pilihan",
+  equalto:        "Konfirmasi password harus sama"
 });
 
 Parsley.setLocale('id');
