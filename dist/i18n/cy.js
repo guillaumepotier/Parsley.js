fc6134cc5bf4b16293a661f7a2c1ae426d028b71
Michael <71503011+Michael-HEIW@users.noreply.github.com>
2021-07-30 13:23:05 +0100

    Add Welsh (cy) (#1358)
    * Add Welsh translations from en v2.9.0

---
+++ dist/i18n/cy.js
@@ -0,0 +1,28 @@
+Parsley.addMessages("cy", {
+    defaultMessage: "Mae'n ymddangos bod y gwerth hwn yn annilys.",
+    type: {
+        email: "Mae angen e-bost dilys.",
+        url: "Dylai'r gwerth hwn fod yn url dilys.",
+        number: "Dylai'r gwerth hwn fod yn rhif dilys.",
+        integer: "Dylai'r gwerth hwn fod yn gyfanrif dilys.",
+        digits: "Dylai'r gwerth hwn fod yn ddigidau.",
+        alphanum: "Dylai'r gwerth hwn fod yn alffaniwmerig."
+    },
+    notblank: "Ni ddylai'r gwerth hwn fod yn wag.",
+    required: "Llenwch y maes hwn os gwelwch yn dda.",
+    pattern: "Mae'n ymddangos bod y gwerth hwn yn annilys.",
+    min: "Dylai'r gwerth hwn fod yn fwy na %s neu'n hafal iddo.",
+    max: "Dylai'r gwerth hwn fod yn is na %s neu'n hafal iddo.",
+    range: "Dylai'r gwerth hwn fod rhwng %s a %s.",
+    minlength: "Mae'r gwerth hwn yn rhy fyr. Dylai fod ganddo %s nod neu fwy.",
+    maxlength: "Mae'r gwerth hwn yn rhy hir. Dylai fod ganddo %s nod neu lai.",
+    length: "Mae'r hyd gwerth hwn yn annilys. Dylai fod rhwng %s a %s nod o hyd.",
+    mincheck: "Rhaid i chi ddewis o leiaf %s dewisiadau.",
+    maxcheck: "Rhaid i chi ddewis dewisiadau %s neu lai.",
+    check: "Rhaid i chi ddewis rhwng %s a %s dewisiadau.",
+    equalto: "Dylai'r gwerth hwn fod yr un fath.",
+    euvatin: "Nid yw'n Rhif Adnabod TAW dilys."
+});
+
+Parsley.setLocale("cy");
+
